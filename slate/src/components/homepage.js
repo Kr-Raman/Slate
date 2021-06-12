5f11a7c334ab263473146c0c9628568ea84a14f3
raghav <raghavjindal131@gmail.com>
2021-06-12 11:13:48 +0530

    js to react ✨

--- slate/src/components/homepage.js
+++ slate/src/components/homepage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import Navbar from "./navbar"
 import boy from "./images/School_Two Color.svg"
-import navbar from "./navbarcss.css"
+import { Form } from "./HomePageScript"
+import "./HomePage.css"
+
+
 class homepage extends Component {
 
 
@@ -17,7 +20,10 @@ class homepage extends Component {
             </p>
             <h3>Preparation examination</h3>
             <h5>Famous teachers gives lectures, say goodbyes to rote memorization, improve grades more efficiently, 1-0n-1 Q&amp;A after className, the course only costs 9 yuan</h5>
-            <button id="signup">Sign Up Free</button>
+            
+           
+            <button id="signup" class="btn--show-modal" onClick={Form}>Sign Up Free</button>
+    
             <button id="learnm">Learn More</button>
         </div>
         <div className="displaypic">
@@ -26,7 +32,29 @@ class homepage extends Component {
     </div>
 
 
-
+    <div>
+  <div className="Signupform hidden">
+    <button className="btn--close-modal">×</button>
+    <h1>Sign Up</h1>
+    <br />
+    <form action className="form">
+      <label htmlFor>
+        First Name
+      </label>
+      <input type="text" />
+      <label htmlFor>
+        Last Name
+      </label>
+      <input type="text" />
+      <label htmlFor>
+        Email
+      </label>
+      <input type="email" />
+      <button className="btn">Next step →</button>
+    </form>
+  </div>
+  <div className="overlay hidden " />
+</div>
       
             </div>
             
